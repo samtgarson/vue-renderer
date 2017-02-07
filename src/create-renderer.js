5f72afae5979b73738fef0f8057946f1fd7d7ff0
Sam Garson <samtgarson@gmail.com>
2017-02-07 20:11:16 +0000

    Small refactor

---
+++ src/create-renderer.js
@@ -0,0 +1,16 @@
+import { createBundleRenderer } from 'vue-server-renderer'
+import pify from 'pify'
+import AWS from 'aws-sdk'
+
+const s3 = new AWS.S3()
+
+export default () => new Promise(async (resolve, reject) => {
+  const data = await s3.getObject({
+    Bucket: process.env.AWS_BUCKET,
+    Key: process.env.AWS_KEY
+  }).promise()
+
+  const bundleRenderer = createBundleRenderer(data.Body.toString())
+  const renderToString = pify(bundleRenderer.renderToString)
+  return resolve(async html => renderToString({ body: html }))
+})
